27925b383e44d2a3f1331192b1d90e4f698c1822
@slovacus <slovacus@gmail.com>
2019-08-14 14:40:04 -0500

    (#1) feat: implement example for structure

---
+++ example/main.tf
@@ -0,0 +1,16 @@
+module "redirect" {
+  source = "github.com/luismayta/terraform-aws-s3-cloudfront-redirect"
+
+  fqdn                = var.fqdn-root
+  redirect_target     = var.fqdn
+  ssl_certificate_arn = data.aws_acm_certificate.domain.arn
+
+  refer_secret = "${base64sha512("SUPER-REFER-SECRET${var.fqdn-root}*AGAIN")}"
+
+  force_destroy = "true"
+
+  providers = {
+    "aws.main"       = aws.main
+    "aws.cloudfront" = aws.cloudfront
+  }
+}
